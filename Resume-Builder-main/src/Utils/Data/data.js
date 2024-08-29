d0f0001e413785015645ce8155a3a442aa2e2bca
sunil kumar <sunilk3469@gmail.com>
2024-08-29 22:41:13 +0530

    added

---
+++ Resume-Builder-main/src/Utils/Data/data.js
@@ -0,0 +1,47 @@
+//Dummy Data for testing purpose
+
+export const data = {
+  personal_info: {
+    first_name: "Sunil",
+    last_name: "Kumar",
+    email: "sunilk3469@gmail.com",
+    mobile: "0123456789",
+    address: "Delhi,india",
+    city: "Delhi",
+    state: "Delhi",
+    postal_code: "110092",
+    objective:
+      "To learn develop applications with CodewithSK",
+  },
+  work_experience: [
+    {
+      id: 1,
+      job_title: "Assistant Manager",
+      organization_name: "SBK IT Solutions",
+      start_year: "2012",
+      end_year: "2024",
+    },
+    {
+      id: 2,
+      job_title: "Customer Support",
+      organization_name: "Alma Better",
+      start_year: "2015",
+      end_year: "2024",
+    },
+    {
+      id: 3,
+      job_title: "Accountant",
+      organization_name: "Alma Better",
+      start_year: "2011",
+      end_year: "2024",
+    },
+  ],
+  education_details: {
+    type: "Post Graduation",
+    university: "IGNOU",
+    degree: "MCA",
+    start_year: "2025",
+    end_year: "2027",
+  },
+  key_skills: ["JAVASCRIPT", "AWS","MERN Stack"],
+};
